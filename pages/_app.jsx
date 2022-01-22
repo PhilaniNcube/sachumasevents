411144c186dcc77c08ffa727a5f60127b2a67802
Philani Ncube <ncbphi001@gmail.com>
2022-01-22 22:45:31 +0200

    google analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,28 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import * as ga from '../lib/ga';
 import Navbar from '../components/Layout/Navbar';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Fragment>
       <Navbar />
